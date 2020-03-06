a3d4a248d491274a77158cc7a2d1b6cde3b78083
onhaxyt <33153580+onhaxyt@users.noreply.github.com>
2020-03-06 11:20:20 +0100

    Updates

---
+++ precache-manifest.70866e109fde9cc0e5dd727216cb8d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcb3c180624a0aa1063153bd9030012",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4b216c23436c7bb3a7c",
+    "url": "./static/js/2.5bc9adc0.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "./static/js/2.5bc9adc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f6dea38e6a31a0c45f",
+    "url": "./static/js/main.173657fe.chunk.js"
+  },
+  {
+    "revision": "aeb3d3382b5f6dbd1ff7",
+    "url": "./static/js/runtime-main.d983b11f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hackernews/precache-manifest.df32a25b3d30e3d80e2d4d340ecf2ce7.js"
+  "./precache-manifest.70866e109fde9cc0e5dd727216cb8d38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackernews/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
